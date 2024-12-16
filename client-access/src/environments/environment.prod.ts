4c18ac13f97159614b0287d363153f9b0a382ad2
Luis Gonzalez <luisarturogonzalezvalencia@gmail.com>
2024-12-16 14:49:01 -0300

    Agregar variables de entorno

---
+++ client-access/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: import.meta.env.NG_APP_FIREBASE_PROJECT_ID,
+    appId: import.meta.env.NG_APP_FIREBASE_APP_ID,
+    storageBucket: import.meta.env.NG_APP_FIREBASE_STORAGE_BUCKET,
+    apiKey: import.meta.env.NG_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.NG_APP_FIREBASE_AUTH_DOMAIN,
+    messagingSenderId: import.meta.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID,
+    measurementId: import.meta.env.NG_APP_FIREBASE_MEASUREMENT_ID,
+  },
+};
