a01e4bddf626c33ad44e3cabde493ad65ed52cc0
Luis Gonzalez <luisarturogonzalezvalencia@gmail.com>
2024-12-19 11:33:32 -0300

    Plans agregado, bulk upload referencia de carpetas

--- client-access/src/app/layouts/admin-layout/admin-layout.component.ts
+++ client-access/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { ToastsContainerComponent } from '../../components/toasts-container/toasts-container.component';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet, ToastsContainerComponent],
+  imports: [RouterOutlet, ToastsContainerComponent, RouterLink],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
 })
